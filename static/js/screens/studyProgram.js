67524f59a0ae2064f1f7f108aa3af92388020283
evakhai <thuyxichlo2k2@gmail.com>
2022-01-20 18:55:49 +0700

    get data from study program page

--- static/js/screens/studyProgram.js
+++ static/js/screens/studyProgram.js
@@ -19,8 +19,59 @@ core.screen = {
                     method: "GET"
                 });
             })
+            
+            makeTree("div", "studyProgram", {
+                table: {
+                    tag: "table",
+                    class: ["generalTable", "noBackground"],
+                    child: {
+                        tag: "thead",
+                        child: {
+                            row: {
+                                tag: "tr",
+                                child: {
+                                    stt: { tag: "th", class: "right", text: "Thứ tự" },
+                                }
+                            }
+                        },
+                        tbody: { tag: "tbody" }
+                    }
+                }
+            });
+        },
 
+        onLogout() {
+			this.reset();
+			this.screen.overlay({
+				icon: "exclamation",
+				title: "Bạn Chưa Đăng Nhập",
+				description: `Hãy đăng nhập vào CTMS để xem nội dung này!`,
+				buttons: {
+					login: { text: "ĐĂNG NHẬP", icon: "signin", onClick: () => core.account.clickable.active = true }
+				}
+			});
 
-        }
+			this.screen.loading = false;
+		},
+
+		async load() {
+			if (!core.account.loggedIn) {
+				this.onLogout();
+				return;
+			}
+
+			try {
+				this.screen.loading = true;
+				await api.results();
+				this.screen.loading = false;
+			} catch(e) {
+				this.reset();
+				this.screen.handleError(e, async () => await this.load());
+
+				this.screen.loading = false;
+			}
+		},
+
+        
     }
 }
